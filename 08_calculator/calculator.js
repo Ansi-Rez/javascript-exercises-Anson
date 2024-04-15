a5a9839b8ded6e39138d98c0ea5f5dd1d1518e02
Anson Reyes <ansonreyes12345@gmail.com>
2024-04-15 16:50:54 -0400

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
-	
+const add = function(numA,numB) {
+  return numA + numB;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA,numB) {
+	return numA - numB;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  if(arr.length !== 0)
+  {
+  for(let i = 0; i < arr.length; i++)
+  {
+    sum += arr[i];
+  }
+  return sum;
+  }
+  else
+  {
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  if(arr.length !== 0)
+  {
+  for(let i = 0; i < arr.length; i++)
+  {
+    sum *= arr[i];
+  }
+  return sum;
+  }
+  else
+  {
+    return sum - 1;
+  }
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  if(num !== 0){
+	for(let i = num; i > 0; i--)
+  {
+    sum *= i;
+  }
+  return sum;
+  }
+  else
+  {
+    return 1;
+  }
 };
 
 // Do not edit below this line
