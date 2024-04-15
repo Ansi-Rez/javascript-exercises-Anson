83c8f25b04bfa925c1999f9133fc886afeb80fa0
Anson Reyes <ansonreyes12345@gmail.com>
2024-04-15 18:14:52 -0400

    Complete palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (raw) {
+    let word = raw.replace(/[.?,! ]/g,"").toLowerCase();
+    let reversearr = word.split("");
+    let reverseword = "";
 
+    for(let i = reversearr.length ; i > 0; i--)
+    {
+        reverseword = reverseword + reversearr[i - 1];
+    }
+
+    if(word === reverseword)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
