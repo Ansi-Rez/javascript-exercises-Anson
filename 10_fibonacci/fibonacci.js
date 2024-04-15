ff89b876c9efa658158ee69f5bc3bcec7c6e6c33
Anson Reyes <ansonreyes12345@gmail.com>
2024-04-15 19:41:07 -0400

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    sequence = [1];
+    if(num == 0)
+    {
+        return 0;
+    }
+    else if(num < 0)
+    {
+        return "OOPS";
+    }
+    else
+    {
+        for(let i = 1; i < num; i++)
+        {
+            if(i === 1)
+            {
+                sequence.push(i);
+            }
+            else
+            {
+                sequence.push(sequence[i-1] + sequence[i-2]);
+            }
+        }
+        return sequence.pop();
+    }
 };
 
 // Do not edit below this line
