39c485381a11e0fc3673dee908b7909b0b25cef1
Anson Reyes <ansonreyes12345@gmail.com>
2024-04-16 00:42:44 -0400

    Complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let booktitles = [];
+
+    for(let i = 0; i < books.length; i++)
+    {
+        booktitles.push(books[i].title);
+    }
+
+    return booktitles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let person = []; 
 
+    for(let i = 0; i < people.length; i++)
+    {
+        if(typeof(people[i].yearOfDeath) === "undefined")
+        {
+            const currentyear = new Date();
+            let name = people[i].name, age = currentyear.getFullYear() - people[i].yearOfBirth;
+            person.push({name,age});
+        }
+        else
+        {
+        let name = people[i].name, age = people[i].yearOfDeath - people[i].yearOfBirth;
+        person.push({name,age});
+        }
+    }
+
+    for(let i = 0; i < person.length; i++)
+    {
+        if(person[0].age < person[i].age)
+        {
+            person[0] = person[i];
+        }
+    }
+
+    return person[0];
 };
 
 // Do not edit below this line
